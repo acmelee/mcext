21addea84efcbdc65d3203e2f27c0933c3ca5f15
dedederek <lihang@starwit.com>
2020-01-02 16:01:43 +0800

    Update extension.ts

--- extension.ts
+++ extension.ts
@@ -1,18 +1,6 @@
 //% weight=94 color=#EC7505 icon="\uf1b3"
 namespace mcext {
 
-// hidden from user, used by other functions
-    //% block
-    function completeTask() {
-        if (!taskIsComplete) {
-            mobs.execute(
-                mobs.target(TargetSelectorKind.NearestPlayer),
-                positions.create(0, 0, 0),
-                "scoreboard players set @s task2success 1"
-            )
-            taskIsComplete = true
-        }
-    }
 
     /**
      * Opens a gate
